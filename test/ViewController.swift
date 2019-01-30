8f81a2fd5a9845e506072dffec54d281d0ea06a1
28cmm <joshuafang1998@gmail.com>
2019-01-30 14:38:33 -0800

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Yilei Huang on 2019-01-30.
+//  Copyright © 2019 Joshua Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
